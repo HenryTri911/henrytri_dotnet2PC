c505a2adc39732d8d2d9d4fc666670a1ed0f289c
HENRY <tongthanhthu911@gmail.com>
2024-10-30 21:27:27 +0700

    push code session5

--- session5/Program.cs
+++ session5/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Bài 1:nhập vào số n từ bàn phím. In ra các số nguyên tố từ 2 đến n
+// Console.Write ("Mời bạn nhập số:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// for (int count=2;count<number;count++)
+// {
+//     bool isPrime = true;
+//     for(int j=2;j<=Math.Sqrt(count);j++)
+//     {
+//         if(count % j == 0)
+//         {
+//             isPrime=false;
+//             break;
+//         }
+//     }
+//     if(isPrime==true)
+//     Console.WriteLine ($"{count}");
+
+// }
+
+//Bài 2: Nhập chiều cao tam giác. In ra tam giác cân rỗng
+ 
+
+
+ //Bài 3: Nhập vào số n từ bàn phím. Kiểm tra số đó có phải là số đôi xứng không
+//  Console.WriteLine ("Mời bạn nhập số:");
+//  int number3 = Convert.ToInt32 (Console.ReadLine());
+//  int original = number3;
+//  int reverseNumber = 0;
+//  while (number3>0)
+//  {
+//     int soCuoi = number3 % 10;
+//     reverseNumber = reverseNumber * 10 + soCuoi;
+//     number3 = number3 / 10;
+//  }
+//  if( reverseNumber == original)
+//  {
+//     Console.WriteLine($" số {original} là số đối xứng");
+
+//  } else 
+//     Console.WriteLine($" Số {original} là số không đối xứng");
+
+
+//
